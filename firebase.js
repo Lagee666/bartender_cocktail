7fc51f0c5edf79388b1db3028d6e3ec6a6da6dad
Lagee666 <z29716581@gmail.com>
2024-03-05 21:20:11 +0800

    Secret

--- firebase.js
+++ firebase.js
@@ -8,14 +8,23 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signO
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC58TlDcuOt-OqSITwG5ehI1AEc066N5jA",
+//     authDomain: "cocktail-adfa8.firebaseapp.com",
+//     projectId: "cocktail-adfa8",
+//     storageBucket: "cocktail-adfa8.appspot.com",
+//     messagingSenderId: "295930608454",
+//     appId: "1:295930608454:web:6da9147968b1d9b51bc347",
+//     measurementId: "G-0XPS44LJRJ"
+// };
 const firebaseConfig = {
-    apiKey: "AIzaSyC58TlDcuOt-OqSITwG5ehI1AEc066N5jA",
-    authDomain: "cocktail-adfa8.firebaseapp.com",
-    projectId: "cocktail-adfa8",
-    storageBucket: "cocktail-adfa8.appspot.com",
-    messagingSenderId: "295930608454",
-    appId: "1:295930608454:web:6da9147968b1d9b51bc347",
-    measurementId: "G-0XPS44LJRJ"
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENTID,
 };
 
 // Initialize Firebase
