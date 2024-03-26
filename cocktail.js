655e7218b5cb623be4deaca30fe18574a5caf9ec
黃裕翔(Ben.YH.Huang) <Ben.YH.Huang@foxtronev.com>
2024-03-26 15:47:02 +0800

    Add material filter

--- cocktail.js
+++ cocktail.js
@@ -1,5 +1,5 @@
 var cocktaildata = {};
-const liquarList = [];
+var liquarList = {};
 var checkedItems = [];
 
 const upload_page_btn = document.getElementById("UploadPageBtn");
@@ -21,6 +21,8 @@ document.addEventListener('DOMContentLoaded', function () {
         .catch(error => {
             console.error('Error fetching JSON:', error);
         });
+        
+    toggleList();
 });
 // 呈現 JSON 數據的函數
 function renderJson(data) {
@@ -35,11 +37,23 @@ function renderJson(data) {
         var rowDiv = document.createElement('div');
         rowDiv.style.display = 'flex';
         rowDiv.style.flexWrap = 'wrap';
-
+        var rowDivCount = 0
         // Iterate through JSON data
-        data.forEach((item, index) => {
+        for (item = 0; item < data.length; item++) {
+            var materialArray = data[item].Material;
+            var materials = materialArray.map(materialItem => materialItem.material);
+            var notAllInlcude = false;
+            for (i = 0; i < checkedItems.length; i++) {
+                if (!materials.includes(checkedItems[i])) {
+                    notAllInlcude = true;
+                    break
+                }
+            }
+            if (notAllInlcude && checkedItems.length != 0) {
+                continue
+            }
             // Check if a new row should be created
-            if (index > 0 && index % 3 === 0) {
+            if (rowDivCount > 0 && rowDivCount % 3 === 0) {
                 // Append the previous rowDiv to cocktailDisplay
                 cocktailDisplay.appendChild(rowDiv);
 
@@ -48,87 +62,94 @@ function renderJson(data) {
                 rowDiv.style.display = 'flex';
                 rowDiv.style.flexWrap = 'wrap';
             }
-
+            rowDivCount++;
             // Create individual itemDiv
-            var materialsArray = item.Material; // 假設 item.Material 是一個包含多個 JSON 物件的陣列
-
-            var materials =item.Material.map(materialItem => materialItem.material);;
-            var allItemsIncluded = checkedItems.every(checkedItem =>
-                materialsArray.some(materialItem => materialItem.material.includes(checkedItem))
-              );
-            if (checkedItems.length === 0 || allItemsIncluded) {
-                const itemDiv = document.createElement('div');
-                itemDiv.innerHTML = `
-            <strong>${item.Name}</strong>:
+            const itemDiv = document.createElement('div');
+            itemDiv.innerHTML = `
+            <strong>${data[item].Name}</strong>:
             <br>
-            第${item.Eposide}集${item.Page}頁
+            第${data[item].Eposide}集${data[item].Page}頁
             <br>
             Material:
             <ul>
-                ${item.Material.map(material => `
+                ${data[item].Material.map(material => `
                 <li>
                     ${material.material}: ${material.value}${material.unit}
                 </li>
                 `).join('')}
             </ul>
             Description: 
-            ${item.Description}
+            ${data[item].Description}
         `;
 
-                // Set itemDiv width to 25%
-                itemDiv.style.width = '33%';
+            // Set itemDiv width to 25%
+            itemDiv.style.width = '33%';
 
-                // Append itemDiv to the current row container
-                rowDiv.appendChild(itemDiv);
-            }
-        });
+            // Append itemDiv to the current row container
+            rowDiv.appendChild(itemDiv);
+
+        }
 
         // Append the last rowDiv (if it contains items) to cocktailDisplay
         if (rowDiv.childElementCount > 0) {
             cocktailDisplay.appendChild(rowDiv);
         }
 
-        getLiquarList(data);
+        getLiquarListandCount(data);
     } else {
         console.error('Error: cocktailDisplay is not defined or null.');
     }
 }
-function getLiquarList(data) {
+function getLiquarListandCount(data) {
+    liquarList = {};
     data.forEach((item, index) => {
         item.Material.forEach((material) => {
-            if (!liquarList.includes(material.material)) {
-                liquarList.push(material.material);
+
+            // console.log(material);
+            if (liquarList[material.material]) {
+                liquarList[material.material]++
+            } else {
+                liquarList[material.material] = 1;
             }
         })
     })
-    toggleList()
+    sortKV();
+    toggleList();
 }
 
+var sortLiquarList = [];
+function sortKV() {
+    // 將 KV 存儲轉換為可排序的數組
+    sortLiquarList = [];
+    for (var material in liquarList) {
+        sortLiquarList.push([material, liquarList[material]]);
+    }
+
+    // 根據值的大小對數組進行排序
+    sortLiquarList.sort(function (a, b) {
+        return b[1] - a[1];
+    });
+}
 
 function toggleList() {
-    var list = document.getElementById("myList");
-    if (list.style.display === "none") {
-        list.style.display = "block";
-
-        var list = document.getElementById("myList");
-        for (var i = 0; i < liquarList.length; i++) {
-          var listItem = document.createElement("li");
-          listItem.textContent = liquarList[i];
-          listItem.onclick = function() {
-            this.classList.toggle("checked");
-            var index = checkedItems.indexOf(this.textContent);
-            if (index === -1) {
-              checkedItems.push(this.textContent);
-            } else {
-              checkedItems.splice(index, 1);
-            }
-            renderJson(cocktaildata);
-          };
-          list.appendChild(listItem);
-        }
-        
-
-    } else {
-        list.style.display = "none";
+    var list = document.getElementById("materialList");
+    // 當列表被收起時，添加列表項目
+    for (var i = 0; i < sortLiquarList.length; i++) {
+        (function(item) {
+            var listItem = document.createElement("li");
+            listItem.textContent = item[0] + '(' + item[1] + ')';
+            listItem.onclick = function () {
+                this.classList.toggle("checked");
+                var index = checkedItems.indexOf(item[0]);
+                if (index === -1) {
+                    checkedItems.push(item[0]);
+                } else {
+                    checkedItems.splice(index, 1);
+                }
+                console.log("勾選了：" + checkedItems);
+                renderJson(cocktaildata);
+            };
+            list.appendChild(listItem);
+        })(sortLiquarList[i]);
     }
 }
